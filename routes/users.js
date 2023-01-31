445392468bb313457220a2af74abc3670d88082d
Your NameNazar <nazarkruk@icloud.com>
2023-01-30 23:37:12 -0500

    Fixing typos

--- routes/users.js
+++ routes/users.js
@@ -67,6 +67,7 @@ router.post("/login", passport.authenticate("local"), (req, res) => {
 });
 
 router.get("/logout", (req, res, next) => {
+  console.log(req.session);
   if (req.session) {
     req.session.destroy();
     res.clearCookie("session-id");
