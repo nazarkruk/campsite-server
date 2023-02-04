b05a961abf6ceb2d5ca7ec26c7ceb17620867cd5
Your NameNazar <nazarkruk@icloud.com>
2023-02-03 19:34:29 -0500

    Debbuging Favorites endpoints

--- models/favorite.js
+++ models/favorite.js
@@ -5,12 +5,12 @@ const favoriteSchema = new Schema(
   {
     user: {
       type: mongoose.Schema.Types.ObjectId,
-      ref: "Campsite",
+      ref: "User",
     },
     campsites: [
       {
         type: mongoose.Schema.Types.ObjectId,
-        ref: "User",
+        ref: "Campsite",
       },
     ],
   },
